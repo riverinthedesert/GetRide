fae0abffb345eef35becce92fb656de11b4ed737
Emeline45 <54811102+Emeline45@users.noreply.github.com>
2021-03-20 20:40:18 +0100

    Visualisation des groupes terminé

--- GetRide/templates/VisuGroupe/index.php
+++ GetRide/templates/VisuGroupe/index.php
@@ -39,58 +39,78 @@ if($conn === false){
 		$groupe = $conn->query("SELECT * FROM `groupemembre` WHERE idUtilisateur='".$_SESSION['idMembre']."'");
 		while($donnees = $groupe->fetch_assoc()){
 			$admOuMembr++;
+			$tabl[] = $donnees['idGroupe'];
 		}
 		if($admOuMembr == 0){ 
 	?>
-	<div class="text-center">
-	<br><br>
-		<div class="p-3 mb-2 bg-info text-white">Aucun groupe d'amis créé !</div>
-	</div>
+			<div class="text-center">
+			<br><br>
+				<div class="p-3 mb-2 bg-info text-white">Aucun groupe d'amis créé !</div>
+			</div>
 	<?php	
 		}
 		else{
+			
 	?>
 	
 	<!-- Sinon :  -->
 		<!-- Boucle pour de :  -->
-		<br>
-		<div class="panel panel-default">
-			<!-- Entete :  -->
-			<div class="panel panel-success">
-				<div class="panel-heading">
-					Nom du groupe 
-					<div class="pull-right">
-						<div class="dropdown">
-							<button class="btn btn-default dropdown-toggle" type="button" id="menu1" data-toggle="dropdown">
-								<span class="glyphicon glyphicon-menu-hamburger"></span>
-							</button>
-							<ul class="dropdown-menu" role="menu" >
-								  <li role="presentation"><a role="menuitem" tabindex="-1" href="#"><span class="glyphicon glyphicon-plus"></span>  Ajouter un ami</a></li>
-								  <li role="presentation"><a role="menuitem" tabindex="-1" href="#"><span class="glyphicon glyphicon-trash"></span>  Supprimer un ami</a></li>
-								  <li role="presentation"><a role="menuitem" tabindex="-1" href="#"><span class="glyphicon glyphicon-pencil"></span>  Modifier le groupe</a></li>
-								  <li role="presentation" class="divider"></li>
-								  <li role="presentation"><a role="menuitem" tabindex="-1" href="#"><p style="color:#FF0000";>Supprimer le groupe</p></a></li>
-							</ul>
+		<?php 
+			foreach($tabl as $element){
+				$name = $conn->query("SELECT * FROM `groupe` WHERE idGroupe='".$element."'");
+				$nom = $name->fetch_assoc();
+		?>
+				<p><br></p>
+				<div class="panel panel-default">
+					<!-- Entete :  -->
+					<div class="panel panel-success">
+						<div class="panel-heading">
+							<?php echo $nom['nom']; ?> 
+							<div class="pull-right">
+								<div class="dropdown">
+									<button class="btn btn-default dropdown-toggle" type="button" id="menu1" data-toggle="dropdown">
+										<span class="glyphicon glyphicon-menu-hamburger"></span>
+									</button>
+									<ul class="dropdown-menu" role="menu" >
+										<li role="presentation"><a role="menuitem" tabindex="-1" href="#"><span class="glyphicon glyphicon-plus"></span>  Ajouter un ami</a></li>
+										<li role="presentation"><a role="menuitem" tabindex="-1" href="#"><span class="glyphicon glyphicon-trash"></span>  Supprimer un ami</a></li>
+										<li role="presentation"><a role="menuitem" tabindex="-1" href="#"><span class="glyphicon glyphicon-pencil"></span>  Modifier le groupe</a></li>
+										<li role="presentation" class="divider"></li>
+										<li role="presentation"><a role="menuitem" tabindex="-1" href="#"><p style="color:#FF0000";>Supprimer le groupe</p></a></li>
+									</ul>
+								</div>
+							</div>
 						</div>
 					</div>
-				</div>
-			</div>
-			<!-- Contenu du panneau : -->
-			<div class="panel-body">
-				<!-- Visualiser la BDD du groupe et afficher les nom et prénom de chaque personne : -->
-				<ul>
-					<li>NOM Prenom</li>
-					<li>NOM2 Prenom2</li>
-					<li>NOM3 Prenom3</li>
-					<li>NOM4 Prenom4</li>
-					<li>NOM5 Prenom5</li>
-					<li>NOM6 Prenom6</li>
-				</ul>
-			</div>
-			<!-- Commentaire donné (optionnel) -->
-			<div class="panel-footer">C'est le meilleur groupe !</div>
-		</div> 
+					<!-- Contenu du panneau : -->
+					<div class="panel-body">
+						<!-- Visualiser la BDD du groupe et afficher les nom et prénom de chaque personne : -->
+						<ul>
+						<?php
+							$groupe = $conn->query("SELECT * FROM `groupemembre` WHERE idGroupe='".$element."'");
+							
+							while($donnees = $groupe->fetch_assoc()){
+								$idUtil = $conn->query("SELECT * FROM `membre` WHERE idMembre='".$donnees['idUtilisateur']."'");
+								while($idMemb = $idUtil->fetch_assoc()){
+
+						?>
+									<li><?php echo $idMemb['nom']; echo $idMemb['prenom'];?> </li>
+						<?php
+								}
+							}
+						?>
+						</ul>
+					</div>
+					<!-- Commentaire donné (optionnel) -->
+					<div class="panel-footer">
+						<?php 
+							if($nom['commentaire'] != 'NULL')
+								echo $nom['commentaire']; 
+						?> 
+					</div>
+				</div> 
 	<?php
+			}
 		}
 	?>
 </div>
