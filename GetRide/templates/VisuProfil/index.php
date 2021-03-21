88717f32741c46ba6d2296eab740c5a957414336
Emeline45 <54811102+Emeline45@users.noreply.github.com>
2021-03-21 16:41:23 +0100

    Ajout de la visu d'un profil

---
+++ GetRide/templates/VisuProfil/index.php
@@ -0,0 +1,69 @@
+<?php
+
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'getride');
+
+$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Vérifier la connexion
+if($conn === false){
+    die("ERREUR : Impossible de se connecter. " . mysqli_connect_error());
+}
+
+	$username = 'trice@gogl.com';
+	
+	$_SESSION['mail'] = $username;
+
+	if(!empty($_SESSION['mail'])){
+?>
+
+<div class="container">
+	<div class="text-center">
+		<h1>Mon Profil</h1>
+	</div>
+	<!-- Recherche dans la BDD-->
+	<div class="input-group">
+        <!-- Je ne sais pas comment faire... -->
+		<img src="profil.jpg"  height="100" width="100" >
+		<!-- Si genre = femme mettre Madame ... sinon Monsieur...-->
+        <?php
+            $membre = $conn->query("SELECT * FROM `membre` WHERE mail='".$_SESSION['mail']."'");
+            while($i = $membre->fetch_assoc()){
+                if($i['genre'] == 'm'){
+                    echo 'Monsieur ';
+                }
+                else{
+                    echo 'Madame ';
+                }
+                echo $i['nom'];
+                echo ' ';
+                echo $i['prenom'];
+                
+        ?>
+	</div>
+	<div class="input-group">
+	    <p><b>Votre email : </b></p>
+        <?php
+            echo $i['mail'];
+        ?>
+	</div>
+	<div class="form-group">
+        <p><b>Votre numéro de téléphone : </b></p>
+        <?php
+            echo $i['telephone'];
+        ?>
+	</div>
+	
+    <a href="#" role="button" class="btn btn-warning "></span>Modifier mot de passe</a>
+	&emsp;&emsp;
+    <a href="#" role="button" class="btn btn-warning "></span>Modifier profil</a>
+	<br><br>
+    <a href="#" role="button" class="btn btn-danger "><span class="glyphicon glyphicon-remove"></span> Fermer mon compte</a>
+	La suppression du compte est définitive !
+</div>
+
+<?php
+    }}
+?>
