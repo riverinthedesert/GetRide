89a8fadade17f3019e57177a45025240fcc8c98d
Emeline45 <54811102+Emeline45@users.noreply.github.com>
2021-04-01 11:10:34 +0200

    Modification de la page d'accueil

--- GetRide/templates/Accueil/index.php
+++ GetRide/templates/Accueil/index.php
@@ -61,8 +61,8 @@ body {
 <div class="container">
 	<div class="text-center">
 		<h3>Vous souhaitez utiliser votre véhicule ? Pas de problème ! </h3>
-		<a href="#" class="btn btn-default btn-lg " role="button" aria-disabled="true"><?php echo $this->Html->image('AjoutPublique.jpg', ['alt' => 'AjoutPublic']); ?><font size="5">Proposer une nouvelle offre de trajet publique</font></a> <br><br>
-		<a href="#" class="btn btn-default btn-lg " role="button" aria-disabled="true"><?php echo $this->Html->image('AjoutPrivée.jpg', ['alt' => 'AjoutPrivee']); ?><font size="5">Proposer une nouvelle offre de trajet privée</font></a>
+		<a href="/GetRide/GetRide/offre/add" class="btn btn-default btn-lg " role="button" aria-disabled="true"><?php echo $this->Html->image('AjoutPublique.jpg', ['alt' => 'AjoutPublic']); ?><font size="5">Proposer une nouvelle offre de trajet publique</font></a> <br><br>
+		<a href="/GetRide/GetRide/offre/add" class="btn btn-default btn-lg " role="button" aria-disabled="true"><?php echo $this->Html->image('AjoutPrivée.jpg', ['alt' => 'AjoutPrivee']); ?><font size="5">Proposer une nouvelle offre de trajet privée</font></a>
 	</div>
 	<br>
 	<div class="container">
@@ -70,22 +70,22 @@ body {
             <div class="col-sm-4">
                 <h3>Ajouter une offre publique</h3>        
                 <p>Vous souhaiter voyager avec votre véhicule mais vous ne <b>connaissez personne ?</b></p>
-                <p><br><a href="#"><span class="glyphicon glyphicon-plus"></span> Ajoutez une offre publique</a> avec toutes vos informations de voyage et les conditions de voyage. Ensuite attendez.</p>
-                <p><br><span class="glyphicon glyphicon-eye-open"></span> Vérifiez vous mail et vos <a href="#">notifications</a> sur le site, si quelqu'un est interressé par votre annonce, vous serez signalé
+                <p><br><a href="/GetRide/GetRide/offre/add"><span class="glyphicon glyphicon-plus"></span> Ajoutez une offre publique</a> avec toutes vos informations de voyage et les conditions de voyage. Ensuite attendez.</p>
+                <p><br><span class="glyphicon glyphicon-eye-open"></span> Vérifiez vos mail et vos <a href="/GetRide/GetRide/notification">notifications</a> sur le site, si quelqu'un est interressé par votre annonce, vous serez signalé
                     et vous pourrez choisir d'accepter cette personne ou de la refuser dans votre voiture.</p>
                 <p><br><span class="glyphicon glyphicon-ok"></span> Et voilà ! Le tour est joué.</p>
             </div>
             <div class="col-sm-4">
                 <h3>Rechercher une offre</h3>
                 <p>Vous souhaiter voyager sans utiliser votre véhicule ou vous n'avez pas de véhicule et ne <b>connaissez personne ?</b></p>
-                <p><br><a href="#"><span class="glyphicon glyphicon-search"></span> Recherchez une offre publique</a>. Filtrez les offres et signalez votre intérêt. Ensuite attendez que votre demande soit validée.</p>
+                <p><br><a href="/GetRide/GetRide/offre"><span class="glyphicon glyphicon-search"></span> Recherchez une offre publique</a>. Filtrez les offres et signalez votre intérêt. Ensuite attendez que votre demande soit validée.</p>
                 <p><br><span class="glyphicon glyphicon-ok"></span> Demande validée. Et voilà le tour est joué. Vous pouvez voyager !</p>
             </div>
             <div class="col-sm-4">
                 <h3>Ajouter une offre privée</h3>
                 <p>Vous avez déjà un compte ? Vous souhaitez voyager avec des personnes que vous connaissez ? </p>
-                <p><br><a href="#"><span class="glyphicon glyphicon-plus"></span> Ajoutez une offre privée</a>, choisissez le groupe de personne à qui envoyer la demande. Et patientez.</p>
-                <p><br><span class="glyphicon glyphicon-eye-open"></span> Vérifiez vous mail et vos  <a href="#">notifications</a> sur le site, si quelqu'un est interressé par votre annonce, vous serez signalé.</p>
+                <p><br><a href="/GetRide/GetRide/offre/add"><span class="glyphicon glyphicon-plus"></span> Ajoutez une offre privée</a>, choisissez le groupe de personne à qui envoyer la demande. Et patientez.</p>
+                <p><br><span class="glyphicon glyphicon-eye-open"></span> Vérifiez vos mail et vos  <a href="/GetRide/GetRide/notification">notifications</a> sur le site, si quelqu'un est interressé par votre annonce, vous serez signalé.</p>
                 <p><br><span class="glyphicon glyphicon-ok"></span> Et voilà ! Le tour est joué.</p>
             </div>
         </div>
